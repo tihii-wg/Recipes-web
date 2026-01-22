3cc16df46de8524db76d4053bee018e409b42b5b
tihii-wg <p0d0lian@mail.ru>
2026-01-22 21:38:06 +0200

    new

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -1,8 +1,9 @@
+// For normal API for code splitting ...
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
   tagTypes: ["myrecipes", "recipes"],
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://dummyjson.com/" }),
   endpoints: () => ({}),
 });
